4e298fc0b8b4c512a6bb60966bc53a64f3d9fa38
sharedordaz <sharedordaz@live.com.mx>
2023-07-18 12:35:21 -0600

    Kool commit

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const path = require('path');
 
+const Artist = require("./models/artists.js");
+
 const jsonparser = express.json();
 const urlparser = express.urlencoded({ extended: true });
 
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+  try {
+    const artists = await Artist.find();
+    res.json(artists);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch artists' });
+  }
 
 
 });
